4fabdcb195d1b7f7643a26e9346dff0d90f198ac
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-29 09:29:58 -0300

    feat: list featured products

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,8 +1,10 @@
 import { env } from "@/env";
 
 export function api(path: string, init?: RequestInit) {
+  const apiPrefix = "/api";
+
   const baseUrl = `${env.NEXT_PUBLIC_API_BASE_URL}`;
-  const url = new URL(path, baseUrl);
+  const url = new URL(apiPrefix.concat(path), baseUrl);
 
   return fetch(url, init);
 }
