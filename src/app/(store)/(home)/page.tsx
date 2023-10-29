cb41ae4cfc9ec58e3ac9b5778475c24800cdc603
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-29 10:06:26 -0300

    chore: remove cache

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -6,9 +6,10 @@ import { Product } from "@/data/types/product";
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api("/products/featured", {
-    next: {
-      revalidate: 60 * 60, // 01 hour
-    },
+    cache: "no-store",
+    // next: {
+    //   revalidate: 60 * 60, // 01 hour
+    // },
   });
 
   const products = await response.json();
