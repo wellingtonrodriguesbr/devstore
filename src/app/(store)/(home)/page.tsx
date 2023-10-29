260b8259cdeed97c58a33278a1fb8f375d8ae096
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-29 09:33:16 -0300

    chore: rename variable

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -12,16 +12,16 @@ async function getFeaturedProducts(): Promise<Product[]> {
 }
 
 export default async function Home() {
-  const [highLightedProduct, ...otherProducts] = await getFeaturedProducts();
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts();
 
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/product/${highLightedProduct.slug}`}
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
       >
         <Image
-          src={highLightedProduct.image}
+          src={highlightedProduct.image}
           className="group-hover:scale-105 transition-transform duration-500"
           width={920}
           height={920}
@@ -30,9 +30,9 @@ export default async function Home() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">{highLightedProduct.title}</span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            {highLightedProduct.price.toLocaleString("pt-br", {
+            {highlightedProduct.price.toLocaleString("pt-br", {
               style: "currency",
               currency: "BRL",
               maximumFractionDigits: 0,
