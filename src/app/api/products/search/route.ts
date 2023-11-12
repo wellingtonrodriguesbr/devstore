d2aec5728e5fdefa5de6719015d459b0a1e4dece
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-11-12 09:05:28 -0300

    feat: api search products

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest } from "next/server";
+
+import data from "../data.json";
+import { z } from "zod";
+
+export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const { searchParams } = request.nextUrl;
+
+  const query = z.string().parse(searchParams.get("q"));
+
+  const products = data.products.filter((p) =>
+    p.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  );
+
+  return Response.json(products);
+}
