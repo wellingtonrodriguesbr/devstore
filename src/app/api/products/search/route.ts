232f645aa27bad2832f4ebf1d4242de33aaacfcd
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-11-18 09:34:42 -0300

    feat: search loading

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -4,7 +4,7 @@ import data from "../data.json";
 import { z } from "zod";
 
 export async function GET(request: NextRequest) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const { searchParams } = request.nextUrl;
 
   const query = z.string().parse(searchParams.get("q"));
